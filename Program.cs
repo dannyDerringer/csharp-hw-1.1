5ad89e485ab019c3bbf1aca052892a6271596739
Danny Derringer <danny.derringer@gmail.com>
2017-05-10 15:30:49 -0400

    Print area of rectangle function
    1. You're tasked with creating some software that should be able to
    calculate the area of a rectangle. Write a single function that accepts
    2 integer parameters and return the calculated area as an integer. In
    your Main function, call your area function passing in same sample data
    to test that your function works. You will be required to print the
    result of your calculation to the screen.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getarea(7,11);
+        }
+
+        static void getarea(int x, int y){
+            int area = x * y; 
+            Console.WriteLine("The area of a 7x11 rectangle is " + area + ".");
+        }
+    }
+
+}
